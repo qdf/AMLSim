4a9a6ad0948bac8a54320878c9745ea425947d68
rcompt <compton@quantiply.com>
2019-07-16 12:30:12 -0700

    Fixing path information

--- pipeline.sh
+++ pipeline.sh
@@ -7,7 +7,7 @@ python scripts/convert_generated_accounts_to_aml_accounts.py
 python scripts/generate_network_in_and_out_degrees.py
 
 echo "GENERATING GRAPH"
-python scripts/transaction_graph_generator.py aml_prop.ini accounts.csv degree.csv paramFiles/transactionType.csv paramFiles/alertPatterns.csv
+python scripts/transaction_graph_generator.py aml_prop.ini outputs/accounts.csv degree.csv paramFiles/transactionType.csv paramFiles/alertPatterns.csv
 echo "DONE"
 
 echo "RUNNING AMLSIM"
