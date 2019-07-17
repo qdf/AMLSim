5c99f18e64af48866e231bd9e905840c0979ce36
rcompt <compton@quantiply.com>
2019-07-17 16:36:08 -0700

    Fixed bug where wrong files were being copied

--- scripts/move_files_to_output_dir.py
+++ scripts/move_files_to_output_dir.py
@@ -23,6 +23,6 @@ cust_files = [os.path.join(file_path,x) for x in os.listdir(file_path) if "custo
 
 latest_cust_csv = max(cust_files,key=os.path.getctime)
 
-copyfile(latest_account_csv, os.path.join(output_dir,latest_cust_csv.split(os.sep)[-1].split(" ")[0]))
+copyfile(latest_cust_csv, os.path.join(output_dir,latest_cust_csv.split(os.sep)[-1].split(" ")[0]))
 
 copyfile("transaction_graph.pkl",os.path.join(output_dir,"transaction_graph.pkl"))
